985d26177573fd3ec072cb2e27cfd2a5230a0ad2
Mostafizur072 <eng.mr.masum@gmail.com>
2024-09-18 18:52:12 +0600

    fgv

--- Asset/js/main.js
+++ Asset/js/main.js
@@ -7,15 +7,14 @@ document.getElementById("downloadButton").addEventListener("click", function() {
     link.download = 'MD.Mostafizur Rahman CV.pdf'; // Replace with the desired file name
     document.body.appendChild(link);
     link.click();
-     
-    
+
     // Remove the link after download
     document.body.removeChild(link);
     
-    // Trigger a function after the PDF is downloaded
-    link.addEventListener('click', function() {
+    // Trigger a function after a slight delay to simulate the download completion
+    setTimeout(function() {
         afterDownload();
-    });
+    }, 1000); // 1000ms (1 second) delay, adjust as needed
 });
 
 function afterDownload() {
