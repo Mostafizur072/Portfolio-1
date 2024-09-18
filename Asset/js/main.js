e34414ceb5c097a088b3407813f9ada36666e4d5
Mostafizur072 <eng.mr.masum@gmail.com>
2024-09-18 19:07:38 +0600

    cv

--- Asset/js/main.js
+++ Asset/js/main.js
@@ -1,27 +1,21 @@
-
-
 document.getElementById("downloadButton").addEventListener("click", function() {
     // Start downloading the PDF file
     const link = document.createElement('a');
-    link.href = '../pdf/MD.Mostafizur Rahman CV.pdf'; // Replace with the actual path to your PDF
-    link.download = 'MD.Mostafizur Rahman CV.pdf'; // Replace with the desired file name
+    link.href = 'Asset/pdf/MD.Mostafizur_Rahman-CV.pdf'; // Replace with the actual path to your PDF
+    link.download = 'MD.Mostafizur_Rahman-CV.pdf'; // Replace with the desired file name
     document.body.appendChild(link);
     link.click();
-
     
-    // Remove the link after download
+    // Remove the link after the download starts
     document.body.removeChild(link);
-    
-    // Trigger a function after a slight delay to simulate the download completion
-    setTimeout(function() {
-        afterDownload();
-    }, 1000); // 1000ms (1 second) delay, adjust as needed
+
+    // Trigger a function to simulate download completion
+    afterDownload();
 });
 
 function afterDownload() {
     alert("PDF Downloaded!"); // Replace this with your desired action
 }
-
 
 //============================================================================>
 
