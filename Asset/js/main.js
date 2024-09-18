a0369ba4eba68c49b9ebb3919df6017b9b04a8e5
Mostafizur072 <eng.mr.masum@gmail.com>
2024-09-18 18:43:54 +0600

    n

--- Asset/js/main.js
+++ Asset/js/main.js
@@ -3,10 +3,11 @@
 document.getElementById("downloadButton").addEventListener("click", function() {
     // Start downloading the PDF file
     const link = document.createElement('a');
-    link.href = 'asset/pdf/MD.Mostafizur Rahman CV.pdf'; // Replace with the actual path to your PDF
+    link.href = '../pdf/MD.Mostafizur Rahman CV.pdf'; // Replace with the actual path to your PDF
     link.download = 'MD.Mostafizur Rahman CV.pdf'; // Replace with the desired file name
     document.body.appendChild(link);
     link.click();
+     
     
     // Remove the link after download
     document.body.removeChild(link);
